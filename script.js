c4156cfd3cd94b91ceb4c3a584df727358d11d30
FelipeIshara <63327220+FelipeIshara@users.noreply.github.com>
2026-04-27 19:18:46 +0000

    projeto 28/04, estagio inicial

--- script.js
+++ script.js
@@ -2,22 +2,39 @@ const taskBtn = document.querySelector("#taskBtn")
 const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
-taskBtn.addEventListener("click", function(){
+function createSpan(taskInputValue){
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    span.addEventListener("click", function(){
+        span.classList.toggle('completed');
+    
+    })
+    return span;
+}
+
+function createDeleteButton(li){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir';
+        button.addEventListener('click', function(){
+        li.remove();
+    })
+    return button;
+}
+
+function addTask(){
     const li = document.createElement('li');
-    const taskInputValue = taskInput.value; 
+    const taskInputValue = taskInput.value;
     if (taskInputValue == ''){
         return
     }
-    const span = document.createElement('span');
-    span.textContent = taskInputValue;
-    const button = document.createElement('button');
-    button.textContent = 'Excluir'
-    button.addEventListener('click', function(){
-        li.remove();
-    })
+
+    const span = createSpan(taskInputValue);
+    const button = createDeleteButton(li);
 
     li.appendChild(span)
     li.appendChild(button)
 
     taskList.appendChild(li);
-})
+}
+
+taskBtn.addEventListener("click", addTask)
